755c5db3353b90eb5acc65bbfe25cbdc296d0fc0
hersheys15 <as474269@ucf.edu>
2025-05-12 15:30:52 -0400

    allows flask app to work w env variables for cloud run

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,8 @@
 # main.py
+import os
 from flask import Flask
+
+port = int(os.environ.get("PORT", 8080))
 app = Flask(__name__)
 
 @app.route("/")
@@ -7,4 +10,4 @@ def home():
     return "Hello from idqsSummer25!"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+    app.run(host="0.0.0.0", port=port)
